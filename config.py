0103cd77074ec54e12b5c69bfcdeb6daa9f0b8a5
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-19 13:06:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+
+
+
 from os import getenv
 
 from dotenv import load_dotenv
@@ -8,24 +11,24 @@ load_dotenv()
 class Config(object):
     LOGGER = True
 
-    API_ID = int(getenv("API_ID", 25395782))
-    API_HASH = getenv("API_HASH", "3c0f6066a07142d664690cfd34447450")
+    API_ID = int(getenv("API_ID", "25475489"))
+    API_HASH = getenv("API_HASH", "3fc2b371f4fbb0166758736414d8be92")
     ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
     SPAMWATCH_API = None
-    TOKEN = getenv("TOKEN", "7519139941:AAE6jFCGiqvhLu1i7HoNL9qdQRZgrQm6HqM")
-    OWNER_ID = int(getenv("OWNER_ID", 1886390680))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "sung_jinwo4")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "souls_societyy")
-    UPDATE_CHAT = getenv("UPDATE_CHAT", "soul_networks")
-    LOGGER_ID = int(getenv("LOGGER_ID", "-1002046276298"))
-    EVENT_LOG = int(getenv("EVENT_LOG", "-1002046276298"))
+    TOKEN = getenv("TOKEN", "7617090520:AAH6OqlzkAGlJUhtd6O3yd9sRfDo6lxwn00")
+    OWNER_ID = int(getenv("OWNER_ID", 1266240012))
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "UnknownX_9_11")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "ANIME_CHAT_ANG")
+    UPDATE_CHAT = getenv("UPDATE_CHAT", "Choco_for_u")
+    EVENT_LOG = int(getenv("EVENT_LOG", "-1002122538649"))
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1002122538649"))
     MONGO_URI = getenv(
         "MONGO_DB_URI",
-        "mongodb+srv://kiraPersonalDB:KiraDB2023@cluster0.qm8bgzb.mongodb.net/",
+        "mongodb+srv://1:1@cluster0.ull4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     )
-    DB_NAME = getenv("DB_NAME", "HoshinoBotV3")
-    REDIS_URL = "redis://default:QuPcQZrXEMAQ6KCrsAKWwL1aQW3gsZEu@redis-16159.c274.us-east-1-3.ec2.cloud.redislabs.com:16159"
-    DATABASE_URL = getenv("DATABASE_URL", "postgres://avnadmin:AVNS_Ez0UFOgjgaSCk-Gr7NQ@sung-erenyeager.h.aivencloud.com:23096/defaultdb?sslmode=require")
+    DB_NAME = getenv("DB_NAME", "ExonRobot")
+    REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
+    DATABASE_URL = getenv("DATABASE_URL", "postgres://avnadmin:AVNS_TbzxTp8QTaLOy_GjvCr@choco-chocoxgithub-f883.j.aivencloud.com:16510/defaultdb?sslmode=require")
 
     # ɴᴏ ᴇᴅɪᴛ ᴢᴏɴᴇ
     if DATABASE_URL.startswith("postgres://"):
@@ -38,3 +41,4 @@ class Production(Config):
 
 class Development(Config):
     LOGGER = True
+                               
