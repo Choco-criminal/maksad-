a5c2266f833e1687bd41f82a23e0d85d1c1c1dfe
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-20 20:27:14 +0530

    Update info.py

--- Exon/modules/info.py
+++ Exon/modules/info.py
@@ -35,28 +35,6 @@ from Exon import DEV_USERS
 from Exon.utils.sections import section
 
 
-async def get_user_info(user, already=False):
-    if not already:
-        user = await app.get_users(user)
-    if not user.first_name:
-        return ["Deleted account", None]
-    user_id = user.id
-    username = user.username
-    first_name = user.first_name
-    mention = user.mention("Link")
-    dc_id = user.dc_id
-    photo_id = user.photo.big_file_id if user.photo else None
-    is_sudo = user_id in DEV_USERS
-    body = {
-        "ID": user_id,
-        "DC": dc_id,
-        "Name": [first_name],
-        "Username": [f"@{username}" if username else None],
-        "Mention": [mention],
-        "Sudo": is_sudo,
-    }
-    caption = section("User info", body)
-    return [caption, photo_id]
 
 
 async def get_chat_info(chat, already=False):
@@ -88,30 +66,6 @@ async def get_chat_info(chat, already=False):
     caption = section("Chat info", body)
     return [caption, photo_id]
 
-
-@app.on_message(filters.command("uinfo"))
-async def info_func(_, message: Message):
-    if message.reply_to_message:
-        user = message.reply_to_message.from_user.id
-    elif len(message.command) == 1:
-        user = message.from_user.id
-    else:
-        user = message.text.split(None, 1)[1]
-
-    m = await message.reply_text("Processing...")
-
-    try:
-        info_caption, photo_id = await get_user_info(user)
-    except Exception as e:
-        return await m.edit(str(e))
-
-    if not photo_id:
-        return await m.edit(info_caption, disable_web_page_preview=True)
-    photo = await app.download_media(photo_id)
-
-    await message.reply_photo(photo, caption=info_caption, quote=False)
-    await m.delete()
-    os.remove(photo)
 
 
 @app.on_message(filters.command("cinfo"))
